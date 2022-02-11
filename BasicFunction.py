38a1d7d2a9056319d93283ef259ee9e71a69618b
Maeva <maeva.beugin@insa-lyon.fr>
2022-02-11 15:00:32 +0100

    Adding docstring and test

--- BasicFunction.py
+++ BasicFunction.py
@@ -1,1 +1,12 @@
-print("Goodbye, World!")
+def printGoodbye():
+    '''
+    Returns the string "Goodbye,World"
+
+            Parameters:
+
+            Returns:
+                The string "Goodbye,World"
+    '''
+    print("Goodbye, World!")
+
+printGoodbye() #Call the function printGoodbye()

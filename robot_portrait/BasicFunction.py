dd6bc2bce9ad7ab3c88a24f4449e2163ab6d4eaa
Danmeierzozo <danmeierzozo@gmail.com>
2022-02-11 07:41:07 -0800

    addition of directory robot_portrait

---
+++ robot_portrait/BasicFunction.py
@@ -0,0 +1,12 @@
+def printGoodbye():
+    '''
+    Returns the string "Goodbye,World"
+
+            Parameters:
+
+            Returns:
+                The string "Goodbye,World"
+    '''
+    print("Goodbye, World!")
+
+printGoodbye() #Call the function printGoodbye()

6c9694f360bc6f6c9399620ed31bfb7bd4b7e989
MarionDufeu <61406209+MarionDufeu@users.noreply.github.com>
2022-03-01 17:11:19 +0100

    Create main.py

---
+++ robot_portrait/main.py
@@ -0,0 +1,30 @@
+#main
+
+def creationPop():#inheritance
+    return 0
+
+def popInitiale():#inheritance
+    return 0
+
+def askWitness():
+    #event.listener click
+    return 0
+
+def main():
+    
+    numberOfGen=0
+    numberMaxOfGen=10
+
+    creationPop()#to take 20 or more random faces from the database
+    askWitness()
+    popInitiale()#to create our intial population
+    #ask the 2 best
+    #encode
+
+    for numberOfGen in range(0, numberMaxOfGen):
+        #encode
+        #call GA
+        #generate a new population
+        #decode to show to the witness
+        #choice
+        #ask what are the 2 best faces

d281c27ca5a0941468d7833cf434d81f8f67a60b
leohermet1 <85879814+leohermet1@users.noreply.github.com>
2022-04-07 19:11:21 +0200

    Update auto_encoder.py

--- robot_portrait/auto_encoder.py
+++ robot_portrait/auto_encoder.py
@@ -32,7 +32,7 @@ def get_pictures(file_str):
     celebA = np.array(celebA)
     return celebA
 
-celebA = get_pictures("/Users/lidanlin/Downloads/Projet_4BIM/Robot_Portrait/robot_portrait/celebA_10000/")
+celebA = get_pictures("~/Robot_Portrait/robot_portrait/celebA_10000/")
 
 
 
